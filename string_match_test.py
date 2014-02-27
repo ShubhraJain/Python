929e30ea2706336ab992ff42a416fb6abce4964e
ShubhraJain <00sankalp@gmail.com>
2014-02-27 13:41:57 -0800

    added another test case

--- string_match_test.py
+++ string_match_test.py
@@ -3,11 +3,12 @@ import string_match
 
 class TestStringMatch(unittest.TestCase):
    def test_stringmatch(self):
-       self.assertEqual(3,string_match.string_match('xxcaazz', 'xxbaaz'))
-       self.assertEqual(2,string_match.string_match('abc', 'abc'))
-       self.assertEqual(0,string_match.string_match('abc', 'axc'))
-       self.assertEqual(0,string_match.string_match('', 'hello'))
-       self.assertEqual(1,string_match.string_match('hello', 'he'))
+       self.assertEqual(3, string_match.string_match('xxcaazz', 'xxbaaz'))
+       self.assertEqual(2, string_match.string_match('abc', 'abc'))
+       self.assertEqual(0, string_match.string_match('abc', 'axc'))
+       self.assertEqual(0, string_match.string_match('', 'hello'))
+       self.assertEqual(1, string_match.string_match('hello', 'he'))
+       self.assertEqual(3, string_match.string_match('abababaaaabbb', 'ab'))
     
 if __name__ == '__main__':
     unittest.main()
