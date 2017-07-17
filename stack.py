c1038caef173998df47712df387eedfca6355a8f
ShubhraJain <00sankalp@gmail.com>
2017-07-16 18:29:45 -0700

    updated names of the file

---
+++ stack.py
@@ -0,0 +1,35 @@
+class stack():
+    """
+    Stack data structure implementation using python
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+        return self.items
+
+    def pop(self):
+        if not self.items:
+            return None
+        else:
+            a = self.items[-1]
+            self.items.pop()
+            return a
+
+    def peek(self):
+        if not self.items:
+            return None
+        else:
+            return self.items[-1]
+
+if __name__ == '__main__':
+    st = stack()
+    print(st.push(1))
+    print(st.push('haha'))
+    print(st.push(3))
+    print(st.pop())
+    print(st.pop())
+    print(st.pop())
+    print(st.pop())
